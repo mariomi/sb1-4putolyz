e97e7ae7ba0f841196c16c1d64b6c0d0af52fc92
Edoardo Sartori <edosartori33@gmail.com>
2025-07-28 20:19:49 +0200

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sb1-4putolyz-main/', // Add base URL for GitHub Pages
+  base: './', // Add base URL for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
