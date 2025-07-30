d4bc3db8fcb3bc7c84e05a8e549e8a6a5dd8f046
Edoardo Portatile <edosartori33@gmail.com>
2025-07-30 16:24:51 +0200

    aa

--- src/pages/CampaignsPage.tsx
+++ src/pages/CampaignsPage.tsx
@@ -923,4 +923,4 @@ export function CampaignsPage() {
                 )}
                 
                 {selectedCampaign.status === 'draft' && (
-                  <div className="px-4 py-2 bg-blue-50 text-blue-700 rounded-lg />
+                  <div className="px-4 py-2 bg-blue-50 text-blue-700 rounded-lg >
